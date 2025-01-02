113523b46f8ae39c62bb90e4cf5439d12560e4b8
Merlinux <master@merlinux.xyz>
2025-01-02 13:26:43 +0100

    mark the added functions commandHelp.go and commandExit.go as usable.

--- main.go
+++ main.go
@@ -6,6 +6,23 @@ import (
 	"os"
 )
 
+var supportedCommands map[string]cliCommand
+
+func init() {
+	supportedCommands = map[string]cliCommand{
+		"exit": cliCommand{
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": cliCommand{
+			name:        "help",
+			description: "Show this help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func main() {
 	buf := bufio.NewScanner(os.Stdin)
 	for {
@@ -13,5 +30,15 @@ func main() {
 		buf.Scan()
 		cleanInput := cleanInput(buf.Text())
 
+		command, ok := supportedCommands[cleanInput[0]]
+		if !ok {
+			fmt.Printf("Unknown command: %s\n", cleanInput[0])
+		}
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Error: %s\n", err)
+			}
+		}
 	}
 }

eee4c4bf490b970dfdc87e04f2bc103501023485
Merlinux <master@merlinux.xyz>
2025-01-02 17:01:32 +0100

    Implement the new commands map and map b.

---
+++ commandMapb.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"boot.dev-Pokedex/internal/pokeapi"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func CommandMapb(conf *CommandConf) error {
+	url := conf.Previous
+	if url == "" {
+		url = pokeapi.MapBaseUrl
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	mapApiResponse := pokeapi.MapApiResponse{}
+	resText, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(resText, &mapApiResponse)
+
+	conf.Next = mapApiResponse.Next
+	conf.Previous = mapApiResponse.Previous
+
+	for _, location := range mapApiResponse.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}

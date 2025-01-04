d70f08d3caab5f2efdfc25d4a48a95cea5402bd6
Merlinux <master@merlinux.xyz>
2025-01-04 13:30:30 +0100

    Add the ability to give arguments to a command.

--- commandConf.go
+++ commandConf.go
@@ -5,4 +5,5 @@ import "boot.dev-Pokedex/internal/pokecache"
 type CommandConf struct {
 	Context   map[string]string
 	HttpCache *pokecache.Cache
+	Argv      []string
 }
--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 			fmt.Printf("Unknown command: %s\n", cleanInput[0])
 		}
 		if ok {
+			command.conf.Argv = cleanInput
 			err := command.callback(command.conf)
 			if err != nil {
 				fmt.Printf("Error: %s\n", err)
